32cf99775d6379b8d3de797106984c2832fcaf7c
BrianBakkala <45982065+BrianBakkala@users.noreply.github.com>
2020-12-31 14:40:53 -0500

    AP Latin Auto HW

--- ControlPanel.php
+++ ControlPanel.php
@@ -52,11 +52,19 @@ hw
 	column-gap:10px;
 	row-gap:10px;
 	border: 2px solid black;
+	cursor:pointer;
 }
+
 hw[suggested="true"]
 {
 	background-color:lightgreen;
 }
+
+hw[suggested="true"][rd="-1"]
+{
+	display:none;
+}
+
 hwtitle
 {
 	display:block;
@@ -70,7 +78,7 @@ hwduedate
 
 </style>
 
-<body onload = 'InitializeCalendarGAPI();'>
+<body onload = 'GetRotationDaysJSON(); InitializeCalendarGAPI();'>
 <?php>
 
 require_once ('GoogleClassroom/APLGSI.php');  
@@ -119,7 +127,15 @@ echo "</div>";
 ?>
 
 
+
 <script>
+const CLIENT_ID = '448443480105-krbg7mnhjqd7s4kdevurs1dtffe1uf1t.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyCN9ZxUhMb9zQW7rK4ZSaP1S4NJ7EKc_es';
+const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest", "https://www.googleapis.com/discovery/v1/apis/classroom/v1/rest"];
+const SCOPES = (["https://www.googleapis.com/auth/calendar.events", "https://www.googleapis.com/auth/calendar.readonly", "https://www.googleapis.com/auth/classroom.topics.readonly",  "https://www.googleapis.com/auth/classroom.coursework.students"].join(" "));
+
+const GoogleClassroomCourseName = "AP Latin E12";
+
 function ToggleTestMode(clickedElement)
 {
 	
@@ -147,29 +163,24 @@ function ToggleTestMode(clickedElement)
 	// return window.location.href.substring(0, window.location.href.lastIndexOf('/')) + "/"  + XMLURL;
 
 }
-</script>
-<BR>
-<BR>
-<BR>
-<BR>
 
-<div data-containertype= "button" style = ' '  onclick = 'SignInWithCheck()' >Link with Google Calendar</div>
+function GetRotationDaysJSON()
+{
 
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function()
+		{
+			if (this.readyState == 4 && this.status == 200)
+			{
+				Response = this.responseText.replace(/(\r\n\t|\n|\r\t)/gm, " ").replace(/^\s+|\s+$/gm, '')
+				document.getElementById('RotationDaysJSON').innerText = Response
+			}
+		};
+		xmlhttp.open("GET", "https://lighthouse.csamsacs.org/Photon/Public/RotationDays.php", true);
+				xmlhttp.send();
 
+}
 
-
-
-
-
-
-
-<script>
-const CLIENT_ID = '448443480105-krbg7mnhjqd7s4kdevurs1dtffe1uf1t.apps.googleusercontent.com';
-const API_KEY = 'AIzaSyCN9ZxUhMb9zQW7rK4ZSaP1S4NJ7EKc_es';
-const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest", "https://www.googleapis.com/discovery/v1/apis/classroom/v1/rest"];
-const SCOPES = (["https://www.googleapis.com/auth/calendar.events", "https://www.googleapis.com/auth/calendar.readonly", "https://www.googleapis.com/auth/classroom.courses.readonly",  "https://www.googleapis.com/auth/classroom.coursework.students"].join(" "));
-
-const GoogleClassroomCourseName = "AP Latin E12";
 
 function InitializeCalendarGAPI()
 {
@@ -248,7 +259,9 @@ function FindClassroomCourse(CourseName)
 					var course = courses[i];
 					if (course.name == CourseName)
 					{
+						COURSEID = course.id;
 						GetClasswork(course.id);
+						GetTopics(course.id);
 					}
 				}
 			}
@@ -289,7 +302,6 @@ function DisplayCurrentHW(HWArray)
 
 		tempHW = document.createElement('hw');
 		tempHW.innerHTML = "<hwtitle>" + HWArray[a]['title'] + "</hwtitle>" 
-		console.log(HWArray[a])
 		tempHW.innerHTML += "<hwduedate>" + HWArray[a]['dueDate']['month'] + "/" + HWArray[a]['dueDate']['day'] + "/" + HWArray[a]['dueDate']['year'] +  "</hwduedate>" 
 		tempHW.innerHTML += "<hwdescription>" + HWArray[a]['description'].substring(0, HWArray[a]['description'].indexOf("\n"))+ "</hwdescription>" 
 		document.getElementById('hwAssigned').appendChild(tempHW)
@@ -317,6 +329,9 @@ function SuggestHW(HWArray, LatestNumber)
 				
 				sd = 0;
 				
+				var tempRDJSON = JSON.parse(document.getElementById('RotationDaysJSON').innerText)
+
+				
 				while ( sd < SheetData.length )
 				{
 					
@@ -328,17 +343,30 @@ function SuggestHW(HWArray, LatestNumber)
 						var tempD =  new Date ((  tempDays *1000*60*60*24))
 						var tempDate = tempD.getFullYear() + "-"+ ("00"+(tempD.getMonth()+1)).slice(-2)+ "-"+ ("00"+tempD.getDate()).slice(-2)
 						var tempHWDueDate =  SheetData[sd+1].content['$t']
-						var tempHWDescription = tempHWbookName+ " "+ SheetData[sd+4].content['$t']
+						var tempHWCitation = tempHWbookName+ " "+ SheetData[sd+4].content['$t']
+						var tempUnit =   SheetData[sd+3].content['$t'].substring(SheetData[sd+3].content['$t'].length-1)
+						var tempRD =   ((tempRDJSON[tempDate] != undefined) ? tempRDJSON[tempDate].RD : -1)
+						var temptimestamp = {
+							"-1":"00:00",
+							"2":"07:55",
+							"4":"07:55",
+							"6":"13:35",
+							"8":"13:35",
+						}[tempRD]+ ":00";
+
 
 						tempHW = document.createElement('hw');
 						tempHW.onclick = function (){CreateAPLatinHWAssignment(this)};
 						tempHW.setAttribute('suggested', 'true')
 						tempHW.setAttribute('datestamp', tempDate)
-						tempHW.setAttribute('description', tempHWDescription)
+						tempHW.setAttribute('timestamp', temptimestamp)
+						tempHW.setAttribute('citation', tempHWCitation)
 						tempHW.setAttribute('hwnum', tempHWNum)
-						tempHW.innerHTML = "<hwtitle>HW" + (+(SheetData[sd].content['$t'].substring(1))) + "</hwtitle>" 
+						tempHW.setAttribute('unit', tempUnit)
+						tempHW.setAttribute('rd', tempRD)
+						tempHW.innerHTML = "<hwtitle>HW" + (+(SheetData[sd].content['$t'].substring(1))) + " ["+tempUnit+"] </hwtitle>" 
 						tempHW.innerHTML += "<hwduedate>" + tempD.toLocaleDateString(undefined) + "</hwduedate>" 
-						tempHW.innerHTML += "<hwdescription>" + tempHWDescription + "</hwdescription>" 
+						tempHW.innerHTML += "<hwdescription>" + tempHWCitation + "</hwdescription>" 
 						document.getElementById('hwAssigned').appendChild(tempHW)
 
 
@@ -354,32 +382,84 @@ function SuggestHW(HWArray, LatestNumber)
 
 }
 
-function CreateAPLatinHWAssignment(clickedElement)
+function GetTopics(ClassId)
 {
-	var descrip = clickedElement.getAttribute('description')
-	var numbah = clickedElement.getAttribute('hwnum')
-	var dateee = clickedElement.getAttribute('datestamp')
+	gapi.client.classroom.courses.topics.list({courseId:ClassId})
+	.then( function(r) { 
+		
+		teTOPICS = (r.result.topic)
+		TOPICS = {};
 
-	///Test Class Id =========    "46640113054"
-
-	console.log(descrip, numbah)
+		for (t=0; t< teTOPICS.length; t++)
+		{
+			TOPICS[+(teTOPICS[t].name.substring(teTOPICS[t].name.indexOf(" "), teTOPICS[t].name.lastIndexOf(" ")))] = teTOPICS[t].topicId
+		}		
+	});
 }
 
+function CreateAPLatinHWAssignment(clickedElement)
+{
+	var citeit = clickedElement.getAttribute('citation')
+	var numbah = clickedElement.getAttribute('hwnum')
+	var dateee = clickedElement.getAttribute('datestamp')
+	var timeee = clickedElement.getAttribute('timestamp')
+	var united = clickedElement.getAttribute('unit')
+
+	var dateobj = (new Date(dateee + " " + timeee + ""))
+	
+	var description = citeit+`
+	http://aplatin.altervista.org/HomeworkViewer.php?hw=`+numbah+`
+	https://docs.google.com/spreadsheets/d/1CKcfxPCIV2Kz7b7QAbhK6JJ5kroxVdZoreGDXvngjS8`
+
+	//Test Class Id =========    "46640113054"
+
+	gapi.client.classroom.courses.courseWork.create({
+		
+		//AP
+		courseId:COURSEID,
+		topicId:TOPICS[united],
+
+		// Test Class
+		// courseId:"46640113054",
+
+
+		workType: "ASSIGNMENT",
+		state: "PUBLISHED",
+		title:("HW"+numbah),
+		description:description,
+		dueDate:{
+				"year": (+(dateee.split('-')[0])),
+				"month": (+(dateee.split('-')[1])),
+				"day": (+(dateee.split('-')[2]))
+				},
+		dueTime:{
+				"hours": dateobj.getUTCHours(),
+				"minutes": dateobj.getUTCMinutes(),
+				"seconds": 0,
+				"nanos": 0
+				},
+		maxPoints:100
+
+		}).then(function(r)
+		
+		{
+			if(r.status == 200)
+			{
+				clickedElement.removeAttribute('suggested')
+			}
+
+		});
+}
 
 
 </script>
 
-
-
-
-
-
-
-
-
 <script async defer src="https://apis.google.com/js/api.js"></script>
 
 
+
+<json style = 'display:none;' id = 'RotationDaysJSON'>
+</json>
 
 
 
@@ -390,22 +470,8 @@ function CreateAPLatinHWAssignment(clickedElement)
 	</div>
 </div>
 
-
-
-
-
-
-
-
-
-
-
-<BR>
-<BR>
-<BR>
-<BR>
-<BR>
 <BR>
 <BR>
 <BR>
 <span onclick = 'signOut()'>signout</span>
+<div data-containertype= "button" style = ' '  onclick = 'SignInWithCheck()' >Link with Google API</div>
